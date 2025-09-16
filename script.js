786df83683deb01c0eae1de82272c0ee08f0ee46
Aman Khan <amankassar790@gmail.com>
2025-09-16 08:51:36 +0530

    added light mode toggle button

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const sideBar = document.querySelector(".side-bar");
 const sideBarIcon = sideBar.querySelector("i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
 const modeBtn = document.querySelector(".toggle-btn-container i");
-const heroSection = document.querySelector("#hero-section");
 
 
 modeBtn.addEventListener("click", () => {
-    heroSection.style.background = "white";
+    document.body.classList.toggle("white-theme");
+    
 })
 
 sideBar.addEventListener("click", () => {
@@ -14,5 +14,7 @@ sideBar.addEventListener("click", () => {
     const isOpen = dropDownMenu.classList.contains("open");
 
     sideBarIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-    
+       
 });
+
+
