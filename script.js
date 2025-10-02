5cded6fd1916b0360d4826128bc34b5c1bea5d5f
Aman Khan <amankassar790@gmail.com>
2025-10-02 20:16:38 +0530

    adding gsap

--- script.js
+++ script.js
@@ -9,6 +9,29 @@ const projectBtn = document.querySelector(".project-toggle i");
 const skillsBtn = document.querySelector(".skills-toggle i");
 const contactBtn = document.querySelector(".contact-toggle i");
 
+// var tl = gsap.timeline();
+
+// tl.from("nav h2,nav ul,nav .toggle-btn-container,.side-bar", {
+//   y: "-40px",
+//   opacity: 0,
+//   duration: 0.7,
+//   // delay: 1,
+//   stagger:0.15
+// });
+// tl.from(".hero-heading,.smalltext",{
+//   x:-150,
+//   opacity:0,
+//   duration:0.8,
+//   stagger:0.15
+// },"-=-0.3")
+
+// tl.from(".hero-footer",{
+//   y:"40px",
+//   opacity:0,
+//   duration:1
+// },"-=4")
+
+
 modeBtn.addEventListener("click", () => {
   document.body.classList.toggle("white-theme");
 });
