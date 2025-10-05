f8c62bec29312645d885dc2096283fe140ab9a71
Aman Khan <amankassar790@gmail.com>
2025-10-05 11:17:04 +0530

    made navbar animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const sideBar = document.querySelector(".side-bar");
-const sideBarIcon = sideBar.querySelector("i");
-const dropDownMenu = document.querySelector(".dropdown-menu");
+const sideBar = document.querySelector(".side-bar i");
+const dropDownMenu = document.querySelector(".dropdown-menu i");
+const navModeBtn = document.querySelector(".mode-btn i");
 const modeBtn = document.querySelector(".toggle-btn-container i");
 const introBtn = document.querySelector(".intro-toggle i");
 const aboutBtn = document.querySelector(".about-toggle i");
@@ -9,28 +9,65 @@ const projectBtn = document.querySelector(".project-toggle i");
 const skillsBtn = document.querySelector(".skills-toggle i");
 const contactBtn = document.querySelector(".contact-toggle i");
 
-var tl = gsap.timeline();
+// GSAP ANIMATION
+function firstPageAnimation() {
+  let tl = gsap.timeline();
+  tl.from("nav h2,nav ul,nav .toggle-btn-container,.side-bar", {
+    y: "-40px",
+    opacity: 0,
+    duration: 0.7,
+    stagger: 0.15,
+  });
+  tl.from(
+    ".hero-heading,.smalltext",
+    {
+      x: -150,
+      opacity: 0,
+      duration: 0.8,
+      stagger: 0.15,
+    },
+    "-=-0.3"
+  );
 
-tl.from("nav h2,nav ul,nav .toggle-btn-container,.side-bar", {
-  y: "-40px",
-  opacity: 0,
-  duration: 0.7,
-  // delay: 1,
-  stagger:0.15
-});
-tl.from(".hero-heading,.smalltext",{
-  x:-150,
-  opacity:0,
-  duration:0.8,
-  stagger:0.15
-},"-=-0.3")
+  tl.from(
+    ".hero-footer",
+    {
+      y: "40px",
+      opacity: 0,
+      duration: 1,
+    },
+    "-=4"
+  );
+}
+function sideBarAnimation() {
+  let tl = gsap.timeline();
+  tl.to(".dropdown-menu", {
+    right: 0,
+    duration: 0.3,
+  });
+  tl.from(".dropdown-menu ul li,.mode-btn", {
+    x: 100,
+    duration: 0.3,
+    stagger: 0.2,
+    opacity: 0,
+  });
+  tl.from(".dropdown-menu ul i", {
+    opacity: 0,
+  });
+  tl.pause();
+  sideBar.addEventListener("click", () => {
+    tl.play();
+  });
 
-tl.from(".hero-footer",{
-  y:"40px",
-  opacity:0,
-  duration:1
-},"-=4")
-
+  dropDownMenu.addEventListener("click", () => {
+    tl.reverse();
+  });
+  navModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("white-theme");
+  });
+}
+firstPageAnimation();
+sideBarAnimation();
 
 modeBtn.addEventListener("click", () => {
   document.body.classList.toggle("white-theme");
@@ -54,9 +91,3 @@ contactBtn.addEventListener("click", () => {
   document.body.classList.toggle("white-theme");
 });
 
-sideBar.addEventListener("click", () => {
-  dropDownMenu.classList.toggle("open");
-  const isOpen = dropDownMenu.classList.contains("open");
-
-  sideBarIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-});
